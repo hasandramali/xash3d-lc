e9f4024c534ba4adb71a0186d13cae74702a7786
hasandramali <93338836+hasandramali@users.noreply.github.com>
2022-12-29 14:36:44 +0200

    Update game.h

--- dlls/game.h
+++ dlls/game.h
@@ -42,9 +42,4 @@ extern cvar_t allowmonsters;
 extern cvar_t *g_psv_gravity;
 extern cvar_t *g_psv_aim;
 extern cvar_t *g_footsteps;
-
-typedef struct
-{
-char		levelname[MAX_QPATH];
-}
 #endif // GAME_H

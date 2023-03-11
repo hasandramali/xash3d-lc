de325b2e92c30016c19be7bfd6faae28af47a302
hasandramali <93338836+hasandramali@users.noreply.github.com>
2023-03-11 14:27:30 +0200

    Update game.cpp

--- dlls/game.cpp
+++ dlls/game.cpp
@@ -874,7 +874,10 @@ void GameDLLInit( void )
 // END REGISTER CVARS FOR SKILL LEVEL STUFF
 
 	SERVER_COMMAND( "exec skill.cfg\n" );
+        if(Q_strcmp(gpGlobals->mapname,"crossfire.bsp"))
+        {
         g_engfuncs.pfnServerCommand("exec crossfirestuff.cfg");
+        }
 }
 
 extern "C"

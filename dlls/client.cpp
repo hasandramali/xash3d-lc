899dbbedf6b9a2b9909fb03208897ff65f7c37cb
hasandramali <93338836+hasandramali@users.noreply.github.com>
2023-11-26 21:06:07 +0200

    Add: unstuck countdown

--- dlls/client.cpp
+++ dlls/client.cpp
@@ -551,9 +551,14 @@ void ClientCommand( edict_t *pEntity )
 	}
 
         else if( FStrEq(pcmd, "unstuck") )
-	{
-		UTIL_CleanSpawnPoint( pev->origin, 150 );
+        {
+                if (!m_flUnstuckCooldown || gpGlobals->time > m_flUnstuckCooldown)
+                {
+                        m_flUnstuckCooldown = gpGlobals->time + 6.0;
+                        UTIL_CleanSpawnPoint( pev->origin, 150 );
+		}
 	}
+
 
 	else if( FStrEq( pcmd, "drop" ) )
 	{

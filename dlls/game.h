4b66f59e5c24e972ff534a504290c44e47ecd468
hasandramali <93338836+hasandramali@users.noreply.github.com>
2024-01-30 23:15:59 +0200

    Update game.h

--- dlls/game.h
+++ dlls/game.h
@@ -37,7 +37,6 @@ extern cvar_t teamlist;
 extern cvar_t teamoverride;
 extern cvar_t defaultteam;
 extern cvar_t allowmonsters;
-extern int AutoJumpValue;
 
 // Engine Cvars
 extern cvar_t *g_psv_gravity;

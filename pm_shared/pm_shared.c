00ccefe89866691184bce48bcb93328e619474f6
hasandramali <93338836+hasandramali@users.noreply.github.com>
2024-01-31 02:04:24 +0200

    Update pm_shared.c

--- pm_shared/pm_shared.c
+++ pm_shared/pm_shared.c
@@ -2712,7 +2712,7 @@ void PM_Jump( void )
 		return;		// in air, so no effect
 	}
 
-	if ( AutoJumpValue && (pmove->oldbuttons & IN_JUMP))
+	if ( autojump.value != 1 && (pmove->oldbuttons & IN_JUMP))
 		return;
 
 	// In the air now.

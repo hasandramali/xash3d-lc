c3a14eb18b173e71a697904c55ae26edd762bcca
hasandramali <93338836+hasandramali@users.noreply.github.com>
2024-08-09 15:24:20 +0300

    Update Application.mk

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,5 +1,5 @@
 CFLAGS_OPT :=  -O3 -fomit-frame-pointer -ggdb -funsafe-math-optimizations -ftree-vectorize -fgraphite-identity -floop-interchange -funsafe-loop-optimizations -finline-limit=256 -pipe
-CFLAGS_OPT_ARM := -mthumb -mfpu=neon -mcpu=cortex-a53 -DVECTORIZE_SINCOS -fPIC -DHAVE_EFFICIENT_UNALIGNED_ACCESS
+CFLAGS_OPT_ARM := -mthumb -mfpu=neon -mcpu=cortex-a9 -pipe -mvectorize-with-neon-quad -DVECTORIZE_SINCOS
 CFLAGS_OPT_ARMv5 :=-march=armv6 -mfpu=vfp -marm -pipe
 CFLAGS_OPT_X86 := -mtune=atom -march=atom -mssse3 -mfpmath=sse -funroll-loops -pipe -DVECTORIZE_SINCOS
 CFLAGS_OPT_ARM64 := -mcpu=cortex-a53 -DVECTORIZE_SINCOS -pipe
